7bfc37d4680c92e6ae37d4d003f754334ee38721
MargotBen <88451082+MargotBen@users.noreply.github.com>
2021-12-10 14:14:01 -0700

    Update FerrisWheel.java

--- FerrisWheel.java
+++ FerrisWheel.java
@@ -1,28 +1,29 @@
 class FerrisWheel extends Attraction{
-FerrisWheel attraction = new FerrisWheel();
-attraction.ridersHour(300);
-attraction.RideSpeed(45.00);
-attraction.RideHeight(60);
-attraction.ticketPerRide(2);
-attraction.rideLength(12);
-attraction.getUtilityCost(40);
-attraction.getPukeFactor(0.99);
-int Romantic;
- 
- //unique method for Ferris Wheel subclass has an equation based on user input to determine romantic factor 
-public int romanticRating()
-{
-if(attraction.getAttendants%2 == 0)
-{
-System.out.println(“Romantic factor is 10/10”);
-}
-else 
-{
-System.out.println(“Romantic factor is 5/10”);
-}
- 
 	
-}
+	 public FerrisWheel(int ticketsToRide, double rideLength, int ridePHour, int rideSpeed) {
+		super(2 , 12, 300, 90);
+	}
+	 AmusementPark connect = new AmusementPark();
+
+
+		int Romantic;
+		 
+		 //unique method for Ferris Wheel subclass has an equation based on user input to determine romantic factor 
+		public void romanticRating()
+		{
+			if(connect.getTickets()%2 == 0)
+			{
+			System.out.println("romantic factor is 10/10");
+			
+			}
+			else 
+			{
+			System.out.println("romantic factor is 5/10");
+			}
+			 
+		}
+ 
+	    }
  
  
  

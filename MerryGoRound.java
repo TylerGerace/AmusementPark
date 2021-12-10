b0bf31df84d674abe6715cc241a3f5629d416ba7
S22400081 <88405954+S22400081@users.noreply.github.com>
2021-12-10 14:04:08 -0700

    Update MerryGoRound.java

--- MerryGoRound.java
+++ MerryGoRound.java
@@ -1,11 +1,15 @@
 class MerryGoRound extends Attraction{ 
 	MerryGoRound attraction = new MerryGoRound();
-attraction.ridersHour(540);	//18 seats * 60 min/2 min ride = 540
-attraction.RideSpeed(8); //Illegal to go 9
+	attraction.ridersHour(540);	//18 seats * 60 min/2 min ride = 540
+	attraction.RideSpeed(8); //Illegal to go 9
 	attraction.RideHeight(2); //Height horse is off the ground
 	attraction.ticketsPerRide(1);
 	attraction.rideLength(2);
-	attraction.getUtilityCost(10);
-	
-	
+	//method for MeeryGoRound that tracks how many times it spins
+	public int spinsPerRide(int spins)
+	{
+		spins = rideLength * 5;
+		return spins;
+	}
+
 }

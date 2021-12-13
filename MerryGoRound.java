6699cfd8ede6fb1e93a0340f9f473b6c96471f1e
MargotBen <88451082+MargotBen@users.noreply.github.com>
2021-12-13 14:02:35 -0700

    Update MerryGoRound.java

--- MerryGoRound.java
+++ MerryGoRound.java
@@ -1,10 +1,13 @@
 class MerryGoRound extends Attraction{
 	
 	 public MerryGoRound(int ticketsToRide, double rideLength, double ridePHour, double rideSpeed,double rideHeight ) {
-		super(1 , 2, 540, 8, 17);
+		super(1 , 4, 120, 8, 17);
 	}
 
-
+	 public double JoyFactor(double joyFactor) {
+			joyFactor = 92.00;
+		    return joyFactor;
+		}
 	
 	
 }

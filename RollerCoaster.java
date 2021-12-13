3926036fc60196c833aa6f83f3e7e60f302cee65
MargotBen <88451082+MargotBen@users.noreply.github.com>
2021-12-13 13:58:53 -0700

    Update RollerCoaster.java

--- RollerCoaster.java
+++ RollerCoaster.java
@@ -1,3 +1,11 @@
-public class RollerCoaster {
-    
+
+public class RollerCoaster extends Attraction{
+	 public RollerCoaster(int ticketsToRide, double rideLength, double ridePHour, double rideSpeed,double rideHeight ) {
+			super(3 , 1.30, 490, 120, 380);
+		} //puke factor method
+public double PukeFactor(double pukeFactor) {
+	pukeFactor = 70.00;
+    return pukeFactor;
+}
+
 }

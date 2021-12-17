323053dd3ef0f2c8a0770d29ffdcef37ab2cc6f2
Tyler <88458575+TylerGerace@users.noreply.github.com>
2021-12-16 23:26:29 -0700

    Update MerryGoRound.java

--- MerryGoRound.java
+++ MerryGoRound.java
@@ -1,13 +1,24 @@
-class MerryGoRound extends Attraction{
-	
-	 public MerryGoRound(int ticketsToRide, double rideLength, double ridePHour, double rideSpeed,double rideHeight ) {
-		super(1 , 4, 120, 8, 17);
-	}
+import javax.swing.JOptionPane;
 
-	 public double JoyFactor(double joyFactor) {
-			joyFactor = 92.00;
-		    return joyFactor;
-		}
-	
-	
+class MerryGoRound extends Attraction{
+    public MerryGoRound(String aName, int cost, double aLength, double aHeight, double aSpeed) {
+        super(aName , cost, aLength, aHeight, aSpeed);
+    }
+    /**
+     * unique method returns a joy factor based of @param age 
+     * @return how much the user enjoyed the ride based off age
+     */
+    public String JoyFactor(int age) {
+        String message = "";
+        if (age <= 12) {message = "\n\nYou greatly enjoyed the ride";}
+        if (age>12&&age<21) {message = "\n\nYou enjoyed the ride";}
+        else {message = "\n\nThe ride was okay";}
+        return message;
+    } 
+    public String Ride(int age){
+        String message = 
+        "You rode "+getName()+" for "+getCost()+" tickets"+JoyFactor(age);
+        JOptionPane.showConfirmDialog(null,message, null, JOptionPane.DEFAULT_OPTION);
+        return "";
+    }
 }

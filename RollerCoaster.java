a55ee86a2f326d3ace18f54366a62301ca076999
Tyler <88458575+TylerGerace@users.noreply.github.com>
2021-12-16 23:26:59 -0700

    Update RollerCoaster.java

--- RollerCoaster.java
+++ RollerCoaster.java
@@ -1,11 +1,28 @@
-
+import javax.swing.JOptionPane;
 public class RollerCoaster extends Attraction{
-	 public RollerCoaster(int ticketsToRide, double rideLength, double ridePHour, double rideSpeed,double rideHeight ) {
-			super(3 , 1.30, 490, 120, 380);
-		} //puke factor method
-public double PukeFactor(double pukeFactor) {
-	pukeFactor = 70.00;
-    return pukeFactor;
-}
-
+    private double length;
+    private double height;
+    private double speed;
+     public RollerCoaster(String aName, int cost, double aLength, double aHeight, double aSpeed) {
+           super(aName , cost, aLength, aHeight, aSpeed);
+           speed = aSpeed;
+           height = aHeight;
+           speed = aSpeed;
+       } 
+    /**
+     * unique method that uses properities to make pukefactor
+     * @return chance based on 60%speed/30%length/10%height
+     */
+    public double PukeFactor() {
+        double pukeFactor;
+        pukeFactor = ((speed*60/100)+(height*10/100)+((length*60/100)*30)-100);
+        return pukeFactor;
+    }
+    public String Ride(){
+        String message = 
+        "You rode "+getName()+" for "+getCost()+" tickets"
+        +"\n\nThere was a "+PukeFactor()+"% chance of you puking";
+        JOptionPane.showConfirmDialog(null,message, null, JOptionPane.DEFAULT_OPTION);
+        return "";
+    }
 }

db9c84657ecde8ee8288a5e514e9f68afe690487
Tyler <88458575+TylerGerace@users.noreply.github.com>
2021-12-16 23:25:07 -0700

    Update Attraction.java

--- Attraction.java
+++ Attraction.java
@@ -1,58 +1,36 @@
-public class Attraction extends AmusementPark {
+
+class Attraction{
     //instance fields
+    private String name; //name of attraction
     private double rideSpeed; //speed of ride ; puke factor ; mph
     private double rideLength; //length of an attraction ; puke factor ; minutes
     private double rideHeight; //height of ride ; puke factor ; feet
     private int ticketsToRide; //ticket cost for an attraction ; profits ; getTickets (tickets that user has)
-    private int totalTickets; //number of tickets purchased ; track users tickets ; 
-    private int ticketSpent; //number of tickets cost by each ride
-    private int riderPerHour;
-    
+
     //Attraction constructor
-    public Attraction(int ticketsToRide,double rideLength,double rideHeight,double rideSpeed, int riderPerHour)
+    public Attraction(String name, int ticketsToRide,double rideLength,double rideHeight,double rideSpeed)
     {
+        this.name = name;
         this.ticketsToRide = ticketsToRide;
         this.rideLength = rideLength;
         this.rideHeight = rideHeight;
         this.rideSpeed = rideSpeed;
-        this.riderPerHour = riderPerHour;
     }
-
-    //Ride Attraction Method
-    public int rideAttraction(int userTickets, Attraction attraction){
-        int tickets = userTickets;
-        return tickets - ticketToRide();
-    }
-
     //get methods
+    /** Methods will @return properties of an attraction*/
+    public String getName() {return name;}
+    public int getCost() {return ticketsToRide;}
+    public double getRideLength() {return rideLength;}
+    public double getRideHeight() {return rideHeight;}
+    public double getRideSpeed() {return rideSpeed;}
 
-    //ticket methods
-    public int numTicket(int ticketPurchased)
-    {
-        totalTickets = ticketPurchased;
-        return totalTickets;
+    //ride Attraction methods (aka toString method)
+    public int buyAttraction(int tickets){
+        return tickets-getCost();
     }
-    public int ticketToRide()
-    {
-        return ticketToRide();
-    }
-    //puke factor method
-    public void PukeFactor(double speed, double length ) {
-        
-    }
-    public int RidersHour(int riders)
-    {
-        riderPerHour = riders;
-        return riderPerHour;
-    }
-    public double RideHeight(int height)
-    {
-        rideHeight = height;
-        return rideHeight;
-    }
-    public double rideLength(double rideTime)
-    {
-        rideLength = rideTime;
-        return rideLength;
+    public String rideAttraction(){
+        String message = 
+        "You rode "+getName()+" for "+getCost()+" tickets";
+        return message;
     }
  }
